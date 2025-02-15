7ecefdaaa94a79916f6ac49ea6eeb8216cf9c75a
Gianluca Napoletano <gianluca.napoletano@cegeka.com>
2025-02-15 18:51:05 +0100

    Init code base

---
+++ TheMovieDataBase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMovieDataBase
+//
+//  Created by Gianluca Napoletano on 15/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
