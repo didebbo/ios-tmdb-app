14d0bffa89fc51becd10c2c8b144e604b7a48084
Gianluca Napoletano <gianluca.napoletano@cegeka.com>
2025-02-17 11:56:32 +0100

    Handle DataProvider

---
+++ TheMovieDataBase/Application/Env/Env.swift
@@ -0,0 +1,12 @@
+//
+//  Env.swift
+//  TheMovieDataBase
+//
+//  Created by Gianluca Napoletano on 17/02/25.
+//
+
+import Foundation
+
+struct Env {
+    static var TMDB_API_Read_Access_Token: String = ""
+}
---
+++ TheMovieDataBase/CoreClass/DataProvider.swift
@@ -0,0 +1,12 @@
+//
+//  DataProvider.swift
+//  TheMovieDataBase
+//
+//  Created by Gianluca Napoletano on 17/02/25.
+//
+
+import Foundation
+
+struct DataProvider {
+    
+}
